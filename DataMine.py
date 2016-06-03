b1714dfff9059bce9647e96ddb9084d467db4d01
esysop <leehassel@gmail.com>
2016-06-03 10:42:53 -0500

    addin to tokens

--- DataMine.py
+++ DataMine.py
@@ -1,5 +1,8 @@
 import tweepy
 import json
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
@@ -14,20 +17,21 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-# def process_or_store():
-#     print(json.dumps(tweet))
+def process_or_store():
+     print(json.dumps(tweet))
 
 def get_user_timeline():
     for status in tweepy.Cursor(api.home_timeline).items(10):
-        print(status.text)
+    print(status.text)
 
-get_user_timeline()
+ get_user_timeline()
 
 
 class MyListener(StreamListener):
     def on_data(self, data):
         try:
             with open('data.json', 'a') as file:
+                print(data)
                 file.write(data)
                 return True
         except BaseException as e:
@@ -40,3 +44,49 @@ class MyListener(StreamListener):
 
 twitter_stream = Stream(auth, MyListener())
 twitter_stream.filter(track=['Trump'])
+
+
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+with open('data.json', 'r') as f:
+    for line in f:
+        tweet = json.loads(line)
+        tokens = preprocess(tweet['text'])
+        #do_something_else(tokens)
+
+tweet = "RT @marcobonzanini: just an example! :D http://example.com #NLP"
+print(preprocess(tweet))
+# ['RT', '@marcobonzanini', ':', 'just', 'an', 'example', '!', ':D', 'http://example.com', '#NLP']
